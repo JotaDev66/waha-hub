a1e47aabebdb523eeb35f60ce8b7e20c31a5b0d2
allburov <allburov@gmail.com>
2024-04-11 12:45:48 +0700

    devtools

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     typescript: false,
+    devtools: {enabled: true},
     app: {
         head: {
             title: 'Sakai Vue',
