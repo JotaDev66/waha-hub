68113c1c62b1aee33a3f6cf177c2099d14463fef
allburov <allburov@gmail.com>
2024-04-19 08:29:55 +0700

    Server dropdown

--- ui/components/RefreshIcon.vue
+++ ui/components/RefreshIcon.vue
@@ -1,10 +1,9 @@
 <script setup>
 const props = defineProps(['refreshing'])
-const icon = computed(() => props.refreshing ? 'pi pi-refresh pi-spin refresh-icon' : 'pi pi-refresh')
 </script>
 
 <template>
-  <ProgressSpinner v-if="true" style="width: 1rem; height: 1rem" stroke-width="4"/>
+  <ProgressSpinner v-if="refreshing" style="width: 1rem; height: 1rem" stroke-width="4"/>
 </template>
 
 <style scoped lang="scss">
