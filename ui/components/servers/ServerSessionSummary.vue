ea1e86733004e97750e4a00eb04296482431ce54
allburov <allburov@gmail.com>
2024-10-16 13:41:21 +0700

    sort sessions

--- ui/components/servers/ServerSessionSummary.vue
+++ ui/components/servers/ServerSessionSummary.vue
@@ -11,11 +11,26 @@ const sessionsByStatus = computed(() => {
   return lodash.groupBy(props.sessions || [], 'status')
 })
 
+// WORKING, SCAN_QR_CODE, STARTING, FAILED, STOPPED
+const statusWeight = {
+  WORKING: 1,
+  SCAN_QR_CODE: 2,
+  STARTING: 3,
+  FAILED: 4,
+  STOPPED: 5
+}
+const sessionsByStatusSorted = computed(() => {
+  return lodash.sortBy(Object.entries(sessionsByStatus.value), ([status, sessions]) => {
+    return statusWeight[status]
+  })
+})
+
+
 </script>
 
 <template>
   <SessionStatusTag
-      v-for="(sessions, status) in sessionsByStatus"
+      v-for="[status, sessions] in sessionsByStatusSorted"
       :key="status + sessions.length"
       :status="status"
       :value="`${status}: ${sessions.length}`"
